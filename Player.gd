1f57f89ed5330627551ca4fef2fb2f8d243bdcd9
jasmaa <jasonmaa3955@gmail.com>
2018-08-18 08:33:11 -0400

    animation and physics work

--- Player.gd
+++ Player.gd
@@ -1,3 +1,7 @@
+"""
+Player control and animation
+"""
+
 extends KinematicBody2D
 
 onready var head_sprite = $Sprites/HeadSprite
@@ -5,46 +9,60 @@ onready var body_sprite = $Sprites/BodySprite
 onready var hand_sprite = $Sprites/HandSprite
 onready var ani = $AnimationPlayer
 
-const MOVE_SPEED = 300
+const WALK_SPEED = 300
+const RUN_SPEED = 800
 const GRAVITY = 10
 const NORM = Vector2(0, -1)
 
-var motion
-var y_vel = 0
-var is_jumping = false
-var is_moving = false
+var speed = WALK_SPEED
+var velocity = Vector2(0, 0)
 var is_flipped = false
+
+enum State{
+	IDLE,
+	MOVING,
+	JUMPING,
+}
+var state = State.IDLE
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
 	if is_on_floor():
-		y_vel = 0
-		is_jumping = false
+		velocity.y = 0
+		state = State.IDLE
 	else:
-		y_vel += GRAVITY
+		velocity.y += GRAVITY
 
 func _process(delta):
-	motion = Vector2(0, 0)
-	is_moving = false
 	
 	# movement
-	if Input.is_action_pressed("move_right"):
-		motion += Vector2(MOVE_SPEED, 0)
-		is_flipped = false
-		is_moving = true
-	if Input.is_action_pressed("move_left"):
-		motion += Vector2(-MOVE_SPEED, 0)
-		is_flipped = true
-		is_moving = true
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		is_jumping = true
-		y_vel = -500
+	if Input.is_action_pressed("dash"):
+		speed = RUN_SPEED
+	else:
+		speed = WALK_SPEED
+		
+	if state != State.JUMPING:
+		velocity.x = 0
+		if Input.is_action_pressed("move_right"):
+			velocity.x = speed
+			is_flipped = false
+			if state != State.JUMPING:
+				state = State.MOVING
+		elif Input.is_action_pressed("move_left"):
+			velocity.x = -speed
+			is_flipped = true
+			if state != State.JUMPING:
+				state = State.MOVING
+		if Input.is_action_pressed("jump") and is_on_floor():
+			state = State.JUMPING
+			velocity += Vector2(0, -500)
+			ani.play("JumpInit")
+		
+	move_and_slide(velocity, NORM)
 	
-	motion += Vector2(0, y_vel)
-	move_and_slide(motion, NORM)
-
+	# hand follows mouse
 	update_hand()
 
 	# animation
@@ -52,11 +70,11 @@ func _process(delta):
 	body_sprite.flip_h = is_flipped
 	hand_sprite.flip_v = is_flipped
 	
-	# temp bool crap
-	if not ani.is_playing() and is_moving and not is_jumping:
+	if not ani.is_playing() and (state == State.IDLE or state == State.MOVING):
 		ani.play("Walk")
-	elif ani.is_playing() and not is_moving or is_jumping:
-		ani.stop()
+	# play landing
+	if is_on_floor() and state == State.JUMPING:
+		ani.play("JumpEnd")
 
 func update_hand():
 	var loc_mouse_pos = get_global_mouse_position()
@@ -66,3 +84,4 @@ func update_hand():
 	# transform
 	hand_sprite.position = loc_mouse_pos.normalized() * 130 + Vector2(0, -50)
 	hand_sprite.rotation_degrees = rot
+				
